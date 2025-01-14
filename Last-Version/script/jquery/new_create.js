3e56a584438f92efc9c022a49f534cc5658aaa4f
Joan Saura <136345085+JoanSaura@users.noreply.github.com>
2025-01-14 19:25:25 +0100

    14

--- Last-Version/script/jquery/new_create.js
+++ Last-Version/script/jquery/new_create.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-    
-    const addRow = $('#add-row');  
+    const addRow = $('#add-row');
 
     addRow.click(function() {
         const choice = parseInt($('#choice').val());  
@@ -8,15 +7,30 @@ $(document).ready(function () {
 
         if (choice === 1) {
             const singleRow = $('<section class="single-row"></section>');
-            const singleElement = $('<div class="single-element"></div>');  
+            const singleElement = $('<div class="single-element"></div>');
+            const EraseBtn = $('<button class="erase-content">Borra</button>');  
+
             singleRow.append(singleElement); 
             newsBody.append(singleRow);  
+            singleRow.append(EraseBtn);  
+
+            EraseBtn.click(function() {
+                singleRow.remove(); 
+            });
+
         } else if (choice === 2) {
             const doubleRow = $('<section class="double-row"></section>');
-            const element1 = $('<div class="double-element element-1"></div>');  
-            const element2 = $('<div class="double-element element-2"></div>');  
+            const element1 = $('<div class="double-element element-1"></div>');
+            const element2 = $('<div class="double-element element-2"></div>');
+            const EraseBtn = $('<button class="erase-content">Borra</button>');  
+
             doubleRow.append(element1).append(element2);  
             newsBody.append(doubleRow);  
+            doubleRow.append(EraseBtn);  
+
+            EraseBtn.click(function() {
+                doubleRow.remove();  
+            });
         }
     });
 });
